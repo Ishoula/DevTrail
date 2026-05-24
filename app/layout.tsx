9149d568ed34563d8526b23ea866dd18cf2ba9b3
Ishoula <shoulamite2k@gmail.com>
2026-05-24 16:00:05 +0200

    Added SpeedInsights for production

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { Providers } from './providers';
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
@@ -19,6 +19,7 @@ export default function RootLayout({
     <html lang="en" className="dark">
       <body className={`${inter.className} antialiased`}>
         <Providers>{children}</Providers>
+        <SpeedInsights/>
       </body>
     </html>
   );
